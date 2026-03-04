e85cf5f7e516d2031cf4dda7a4ff3156b6deccde
unknown <highwayhellman@gmail.com>
2026-03-04 15:01:45 +0300

    index page

--- hexlet_code/page_analyzer/app.py
+++ hexlet_code/page_analyzer/app.py
@@ -1,11 +1,15 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template, request
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret')  # секрет только из env!
 
-@app.route('/')
-def hello():
-    return '<h1>Page Analyzer</h1>'
+@app.route("/")
+def index():
+    if request.method == "POST":
+        url = request.form["url"]
+        # Пока просто редирект (позже обработаем)
+        return f"<h1>Анализ {url}</h1>"  # заглушка
+    return render_template("index.html")
