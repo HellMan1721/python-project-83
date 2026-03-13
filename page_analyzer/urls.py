28e4c4bd2142f2fe8c5c1da1d380641a9926796e
HellMan1721 <highwayhellman@gmail.com>
2026-03-13 18:48:29 +0300

    thangs

--- page_analyzer/urls.py
+++ page_analyzer/urls.py
@@ -9,7 +9,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+
 class URL:
+    @staticmethod
+    def init_db():
+        """Создание таблиц для URL."""
+        with URL.get_connection() as conn:
+            conn.execute("""
+                CREATE TABLE IF NOT EXISTS urls (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) UNIQUE NOT NULL,
+                    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+                );
+                
+                CREATE TABLE IF NOT EXISTS url_checks (
+                    id SERIAL PRIMARY KEY,
+                    url_id INTEGER NOT NULL REFERENCES urls (id),
+                    status_code INTEGER,
+                    h1 VARCHAR(255),
+                    title VARCHAR(255),
+                    description TEXT,
+                    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+                );
+            """)
+            conn.commit()
+            
     @staticmethod
     def get_connection():
         return psycopg.connect(os.getenv("DATABASE_URL"))
